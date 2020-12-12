3e6687fac8b1fc75628934131ac47c5fa9ad0910
Brandon Piercy <b.piercy567@gmail.com>
2020-12-12 10:44:10 -0800

    updated the burger model with a skeleton, started the burger controller and started the orm.js file in the config

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,20 @@
 var connection = require("../config/connection.js");
 
+var orm = {
+    selectAll: function(cb) {
+        var queryString = "SELECT * FROM burgers;";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(value) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES ('?')";
+        connection.query(queryString, value, function(err,result) {
+            if (err) throw err;
+            console.log(Value + " added successfully");
+            console.log(result);
+        });
+    },
+    updateOne: function()
+}
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,15 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll(function(res) {
+            cb(res);
+        });
+    },
+    create: function() {
+
+    },
+    update: function() {
+
+    }
+};
