8ac0a898faae37fb5ae27653e475ede637582d72
Brandon Piercy <b.piercy567@gmail.com>
2020-12-12 09:44:21 -0800

    created a schema and seeds for the mySQL server

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Big Mac");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Black and Bleu Burger");
