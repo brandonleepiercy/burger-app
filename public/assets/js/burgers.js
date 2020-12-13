f36c2db5427bdef3df845a58385b0f87f3431413
Brandon Piercy <b.piercy567@gmail.com>
2020-12-12 19:18:59 -0800

    added code to the burgers.js file in the public assets, does not function properly

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,33 @@
+$(function() {
+
+    $(".devour-butt").on("click", function(event) {
+        var id = $(this).data("id");
+        $.ajax("/api/burgers/"+id, {
+            type:"PUT"
+        }).then(
+            function() {
+                console.log("changed id: "+id+" to devoured.");
+
+                location.reload();
+            }
+        ).catch(err => {if (err) throw err});
+    });
+
+    $(".burg-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burg-name").val.trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created "+newBurger.burger_name);
+                location.reload();
+            }
+        ).catch(err => {if (err) throw err});
+    });
+});
